498c384d05b2589e80bca24b59e1cca888428c80
pedrolrm <p.lucarocha@gmail.com>
2025-09-24 17:21:48 -0300

    Cria a classe consulta e  a lista de possíveis estados da consulta

---
+++ src/entidades/Consulta.java
@@ -0,0 +1,54 @@
+package entidades;
+import java.time.LocalDateTime;
+
+public class Consulta {
+    private Paciente paciente;
+    private Medico medico;
+    private LocalDateTime horario;
+    private String local;
+    private StatusConsulta status;
+
+    public Consulta(Paciente paciente, Medico medico, LocalDateTime horario, String local){
+        this.paciente = paciente;
+        this.medico = medico;
+        this.horario = horario;
+        this.local = local;
+        this.status = StatusConsulta.AGENDADA;
+    }
+
+    public Paciente getPaciente(){
+        return paciente;
+    }
+
+    public Medico getMedico(){
+        return medico;
+    }
+
+    public LocalDateTime getHorario(){
+        return horario;
+    }
+
+    public String getLocal(){
+        return local;
+    }
+
+    public StatusConsulta getStatus(){
+        return status;
+    }
+
+    public void setStatus(StatusConsulta novoStatus){
+        this.status = novoStatus;
+    }
+
+     @Override
+    public String toString() {
+        return "Consulta {" +
+                "\n  Paciente: " + paciente +
+                ",\n  Medico: " + medico +
+                ",\n  Data/Hora: " + horario +
+                ",\n  Local: '" + local + '\'' +
+                ",\n  Status: " + getStatus() +
+                "\n}";
+    }
+    
+}
---
+++ src/entidades/StatusConsulta.java
@@ -0,0 +1,7 @@
+package entidades;
+
+public enum StatusConsulta {
+    AGENDADA,
+    CONCLUIDA,
+    CANCELADA
+}
